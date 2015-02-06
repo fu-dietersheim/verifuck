84da886be4bd00915b2e3895680592650fd5b5af
Lars Hupel <lars.hupel@mytum.de>
2015-02-06 21:31:00 +0100

    parser, runner

--- thy/Verifuck.thy
+++ thy/Verifuck.thy
@@ -26,13 +26,26 @@ end
 
 datatype_new instr = Incr | Decr | Right | Left | Out | In | Loop | Pool
 
+fun parse_instrs :: "string \<Rightarrow> instr list" where
+"parse_instrs [] = []" |
+"parse_instrs (x # xs) = (
+  if x = CHR ''.'' then Out # parse_instrs xs else
+  if x = CHR '','' then In # parse_instrs xs else
+  if x = CHR ''+'' then Incr # parse_instrs xs else
+  if x = CHR ''-'' then Decr # parse_instrs xs else
+  if x = CHR ''<'' then Left # parse_instrs xs else
+  if x = CHR ''>'' then Right # parse_instrs xs else
+  if x = CHR ''['' then Loop # parse_instrs xs else
+  if x = CHR '']'' then Pool # parse_instrs xs else
+  parse_instrs xs)"
+
 datatype_new 'a tape = Tape (left: "'a list") (cur: 'a) (right: "'a list")
 
 definition empty_tape :: "'a::zero tape" where
 "empty_tape = Tape [] 0 []"
 
 definition tape_map_cur :: "('a \<Rightarrow> 'a) \<Rightarrow> 'a tape \<Rightarrow> 'a tape" where
-"tape_map_cur f tape = Tape (left tape) (f (cur tape)) (right tape)"
+[simp]: "tape_map_cur f tape = Tape (left tape) (f (cur tape)) (right tape)"
 
 fun tape_shift_right :: "'a::zero tape \<Rightarrow> 'a tape" where
 "tape_shift_right (Tape l c []) = Tape (c # l) 0 []" |
@@ -50,10 +63,10 @@ definition init_io :: "'a list \<Rightarrow> ('a, 'a list) io" where
 type_synonym ('a, 'b) machine = "'a tape \<times> ('a, 'b) io"
 
 definition read_io :: "('a, 'b) io \<Rightarrow> ('a \<times> ('a, 'b) io)" where
-"read_io io = (let (c, state') = read io (state io) in (c, Buffer state' (read io) (out_buf io)))"
+[simp]: "read_io io = (let (c, state') = read io (state io) in (c, Buffer state' (read io) (out_buf io)))"
 
 definition write_io :: "'a \<Rightarrow> ('a, 'b) io \<Rightarrow> ('a, 'b) io" where
-"write_io c io = Buffer (state io) (read io) (c # out_buf io)"
+[simp]: "write_io c io = Buffer (state io) (read io) (c # out_buf io)"
 
 type_synonym instr_table = "instr list \<times> instr list list"
 
@@ -78,16 +91,38 @@ fun skip_loop :: "instr list \<Rightarrow> nat \<Rightarrow> instr list" where
 partial_function (tailrec) interp_bf :: "instr_table \<Rightarrow> ('a::{zero,one,minus,plus}, 'b) machine \<Rightarrow> ('a, 'b) machine" where
 "interp_bf tab m =
   (case tab of ([], _) \<Rightarrow> m |
-               (Loop # is, stack) \<Rightarrow> if cur (fst m) = 0 then interp_bf (skip_loop is 1, stack) m else interp_bf (is, is # stack) m |
+               (Loop # is, stack) \<Rightarrow> if cur (fst m) = 0 then interp_bf (skip_loop is 1, stack) m else interp_bf (is, (Loop # is) # stack) m |
                (Pool # _, is2 # stack) \<Rightarrow> interp_bf (is2, stack) m | (* todo: codegen bug, rename is2 to is to reproduce *)
                (Pool # _, []) \<Rightarrow> m |
                (i # is, stack) \<Rightarrow> interp_bf (is, stack) (next_machine i m))"
 
 declare interp_bf.simps[code]
 
-export_code interp_bf in SML module_name Verifuck file "code/verifuck.ML"
+definition run_bf :: "instr list \<Rightarrow> 'a::{zero,one,minus,plus} list \<Rightarrow> 'a list" where
+"run_bf prog input = rev (out_buf (snd (interp_bf (init_table prog) (empty_tape, init_io input))))"
+
+export_code run_bf in SML module_name Verifuck file "code/verifuck.ML"
 (*SML_file "code/verifuck.ML"*)
 
-value (code) "interp_bf (init_table [Incr, Left, Incr, Left, In, Out]) (empty_tape :: int tape, init_io [2])"
+definition "hello_world = ''++++++++++
+ [
+  >+++++++>++++++++++>+++>+<<<<-
+ ]                       Schleife zur Vorbereitung der Textausgabe
+ >++.                    Ausgabe von 'H'
+ >+.                     Ausgabe von 'e'
+ +++++++.                'l'
+ .                       'l'
+ +++.                    'o'
+ >++.                    Leerzeichen
+ <<+++++++++++++++.      'W'
+ >.                      'o'
+ +++.                    'r'
+ ------.                 'l'
+ --------.               'd'
+ >+.                     '!'
+ >.                      Zeilenvorschub
+ +++.                    Wagenruecklauf''"
+
+value (code) "run_bf (parse_instrs hello_world) ([]::char list)"
 
 end
